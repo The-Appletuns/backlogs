d8ed767204fc5bace9400817f7ec9723f7772363
stefxici <97343745+stefxici@users.noreply.github.com>
2023-09-19 15:32:14 -0700

    created new controller

---
+++ Controllers/EstefController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backlogs.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestingController : ControllerBase
+{
+    private readonly ILogger<TestingController> _logger;
+
+    public TestingController(ILogger<TestingController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<UserData> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new UserData
+        {
+            Id = index,
+            Username = new string("Appletun" + index),
+            Email = new string("Appletun" + "@gmail.com")
+        })
+        .ToArray();
+    }
+}
