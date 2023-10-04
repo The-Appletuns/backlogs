c3aec89aea21a0a32daf3e5e62c8789955923bd6
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2023-10-04 14:11:43 -0700

    added start for profile section

--- ClientApp/src/components/UserProfile.js
+++ ClientApp/src/components/UserProfile.js
@@ -3,39 +3,84 @@ import React, { Component } from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-import PersonIcon from '@mui/icons-material/Person'
+import PersonIcon from '@mui/icons-material/Person';
 
 
 export class UserProfile extends Component {
     static displayName = UserProfile.name;
 
+    userProfileHeader() {
+        // User info at the header
+
+        return (
+            <Stack direction="row" alignItems='center' justifyContent='space-between'>
+                <Stack 
+                    direction="row" 
+                    alignItems='center' 
+                    justifyContent='space-around'
+                    spacing={4}>
+                    <PersonIcon fontSize='large'/>
+                    <Typography variant='h2'>Username</Typography>
+                </Stack>
+                <Stack 
+                    direction="row"
+                    alignItems='center'
+                    divider={<Divider orientation="vertical" flexItem/>}
+                    spacing={2}>
+                    <Box>
+                        <Typography variant='h5'>##</Typography>
+                        <Typography variant='h5'>Games</Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant='h5'>##</Typography>
+                        <Typography variant='h5'>Following</Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant='h5'>##</Typography>
+                        <Typography variant='h5'>Followers</Typography>
+                    </Box>
+                </Stack>
+            </Stack>
+        )
+    }
+
+    userProfileBody() {
+        // User profile displaying games and other info
+
+        return (
+            <Stack
+                alignItems='stretch'
+                divider={<Divider orientation="horizontal" flexItem/>}
+                spacing={4}>
+                <Box>
+                    <Typography variant='h4'>Favorite Games</Typography>
+
+                </Box>
+                <Box>
+                    <Typography variant='h4'>Recent Games</Typography>
+                    
+                </Box>
+                <Box>
+                    <Typography variant='h4'>Recent Reviews</Typography>
+                    
+                </Box>
+                <Box>
+                    <Typography variant='h4'>Collection Games</Typography>
+                    
+                </Box>
+            </Stack>
+        )
+    }
+
     render() {
         return (
             <Box>
-                <Stack direction="row" alignItems={'baseline'}>
-                    <Stack direction="row">
-                        <PersonIcon/>
-                        <h1>Username</h1>
-                    </Stack>
-                    <Stack 
-                        direction="row"
-                        divider={<Divider orientation="vertical" flexItem/>}
-                        spacing={2}>
-                        <Box>
-                            <h2>##</h2>
-                            <h2>Games</h2>
-                        </Box>
-                        <Box>
-                            <h2>##</h2>
-                            <h2>Following</h2>
-                        </Box>
-                        <Box>
-                            <h2>##</h2>
-                            <h2>Followers</h2>
-                        </Box>
-                    </Stack>
-                </Stack>
+                {this.userProfileHeader()}
+                <Divider/>
+                {this.userProfileBody()}
             </Box>
         )
     }
