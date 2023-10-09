b01c77272bed597302106355647de6c9de047da3
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2023-10-09 11:42:02 -0700

    started moby games controller

---
+++ Controllers/MobyGamesController.cs
@@ -0,0 +1,13 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace backlogs.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MobyGamesController : ControllerBase
+{
+
+    
+}
