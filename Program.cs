cd8a09350c0a1dbac315679bbdb7c88f7a5a890a
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2023-11-02 09:17:54 -0700

    fixing up startup file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using backlogs.Models;
 using backlogs.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,26 @@ builder.Services.AddSingleton<UsersService>();
 builder.Services.AddControllers()
     .AddJsonOptions(
         options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+// JWT Authentication Service
+// builder.Services.AddAuthentication(x =>
+//     {
+//         x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//         x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//     }
+// )
+// .AddJwtBearer(x => 
+// {
+//     x.RequireHttpsMetadata = false;
+//     x.SaveToken = true;
+//     x.TokenValidationParameters = new TokenValidationParameters
+//     {
+//         ValidateIssuerSigningKey = true,
+//         // IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(IConfiguration.GetSection("JwtKey").ToString())),
+//         ValidateIssuer = false,
+//         ValidateAudience = false
+//     };
+// });
 
 var app = builder.Build();
 
---
+++ Startup.cs
@@ -0,0 +1,49 @@
+using backlogs.Models;
+using backlogs.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+
+        services.Configure<BackLogsDatabaseSettings>(Configuration.GetSection("BackLogsDatabase"));
+        services.AddSingleton<UsersService>();
+        // Add more services here as needed
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (!env.IsDevelopment())
+        {
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
