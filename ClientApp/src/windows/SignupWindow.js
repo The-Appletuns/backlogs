bef5d9add3014a4befed1d3d9275c355ebaedb10
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2023-11-09 09:34:03 -0800

    sign up window testing

--- ClientApp/src/windows/SignupWindow.js
+++ ClientApp/src/windows/SignupWindow.js
@@ -1,1 +1,44 @@
+
+
 // Sign up window
+export default function signupWindow() {
+
+    return (
+        <Box>
+            <Typography variant='h3'>
+                Sign up for BackLogs!
+            </Typography>
+
+            <FormControl>
+                <TextField
+                    required
+                    label='Username'
+                    variant='outlined'></TextField>
+
+                <TextField
+                    required
+                    label='Email'
+                    variant='outlined'></TextField>
+
+                <TextField
+                    required
+                    label='First Name'
+                    variant='outlined'></TextField>
+
+                <TextField
+                    label='Last Name'
+                    variant='outlined'></TextField>
+
+                <TextField
+                    required
+                    label='Password'
+                    variant='outlined'></TextField>
+            </FormControl>
+
+            <Button variant='contained'>Sign Up</Button>
+
+            <Typography variant='h5'>Already have an account?</Typography>
+            <Button variant='outlined'>Login</Button>
+        </Box>
+    )
+}
