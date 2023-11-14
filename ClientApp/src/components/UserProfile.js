0271355bb97874e092555e849ef4d76076dc9d80
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-11-14 06:02:02 -0800

    deploeyd on new site

--- ClientApp/src/components/UserProfile.js
+++ ClientApp/src/components/UserProfile.js
@@ -50,7 +50,7 @@ class UserProfile extends Component {
             return;
         }
 
-        const dbAccess = 'http://ec2-54-183-127-27.us-west-1.compute.amazonaws.com/api/user/' + userID;
+        const dbAccess = 'http://vgbacklogs.com/api/user/' + userID;
         const authToken = 'Bearer ' + token;
 
         try {
