f7fefa24d972ec409ba2ee257cdad87026fcb542
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2023-11-14 16:49:48 -0800

    Create UserListDisplay.js

---
+++ ClientApp/src/windows/UserListDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+// User list display
+export default function userListDisplay() {
+    return(
+        <Box>
+            <Image>
+                {/* Image here */}
+            </Image>
+            <Typography>
+                {/* Username here */}
+            </Typography>
+        </Box>
+    )
+}
